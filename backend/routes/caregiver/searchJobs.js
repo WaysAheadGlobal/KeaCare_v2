3aa6b6af2ded78c906ddd8d9b114b0a5d0daa756
Niladitya-Sen <niladityasen.2212@gmail.com>
2023-08-15 23:50:17 +0530

    bug fix search job

--- backend/routes/caregiver/searchJobs.js
+++ backend/routes/caregiver/searchJobs.js
@@ -9,13 +9,20 @@ async function searchJobs(req, res) {
 
     connection.query(`
         SELECT jobs_.*, careseekers_.imageUrl, careseekers_.fname, careseekers_.lname FROM jobs_ INNER JOIN careseekers_ WHERE 
-        ${speciality ? `jobs_.speciality = '${speciality} AND '` : ''} 
-        ${pet ? `jobs_.comfortableWithPets = 1 AND ` : ''} 
-        ${rate ? `jobs_.hourlyRate <= ${rate} AND ` : ''}
-        ${age ? `jobs_.age <= ${age} AND ` : ''}
-        ${experience ? `jobs_.experience >= ${experience} AND ` : ''}
-        ${languages ? `LOCATE('${languages}', jobs_.language) != 0 AND ` : ''}
-        ${addservices ? `LOCATE('${addservices}', jobs_.additionalService) != 0 AND ` : ''}
+        ${speciality ? `jobs_.speciality = '${speciality}'` : ''} 
+        ${speciality && (pet || rate || age || experience || languages || addservices || rating) ? ' AND ' : ''}
+        ${pet ? `jobs_.comfortableWithPets = 1` : ''} 
+        ${pet && (rate || age || experience || languages || addservices || rating) ? ' AND ' : ''}
+        ${rate ? `jobs_.hourlyRate <= ${rate}` : ''}
+        ${rate && (age || experience || languages || addservices || rating) ? ' AND ' : ''}
+        ${age ? `jobs_.age <= ${age}` : ''}
+        ${age && (experience || languages || addservices || rating) ? ' AND ' : ''}
+        ${experience ? `jobs_.experience >= ${experience}` : ''}
+        ${experience && (languages || addservices || rating) ? ' AND ' : ''}
+        ${languages ? `LOCATE('${languages}', jobs_.language) != 0` : ''}
+        ${languages && (addservices || rating) ? ' AND ' : ''}
+        ${addservices ? `LOCATE('${addservices}', jobs_.additionalService) != 0` : ''}
+        ${addservices && rating ? ' AND ' : ''}
         ${rating ? `jobs_.rating >= ${rating}` : ''}
         AND careseekers_.id = jobs_.userId
         AND jobs_.status = 'active'
